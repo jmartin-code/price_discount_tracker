d3142cb207ecf8bff24b5d9a3af76556a2c8b0fb
jmartin-code <jmmartinez331@gmail.com>
2021-11-02 16:03:55 -0400

    adding more to the item and user model

--- server/index.js
+++ server/index.js
@@ -2,15 +2,67 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// error handler
+
+///////////// DB Section ///////////////
+const { Sequelize, STRING, TEXT, DECIMAL } = require('sequelize')
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:fullstack25@localhost/test', { logging: false })
+
+//////////// Models //////////////////
+const User = db.define('user', {
+    name: {
+        type: STRING
+    },
+    email: {
+        type: STRING,
+        unique: true
+    },
+    password: {
+        type: STRING,
+    }
+})
+
+const Item = db.define('item', {
+    name: {
+        type: STRING
+    },
+    link: {
+        type: TEXT
+    },
+    imageURL: {
+        type: TEXT
+    },
+    price: {
+        type: DECIMAL
+    },
+    targetPrice: {
+        type: DECIMAL
+    }
+})
+
+const syncAndSeed = async () => {
+    try {
+        await db.authenticate();
+        console.log('DB authenticated')
+        await db.sync({ force: true })
+        console.log('DB Ready!')
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+
 const init = async () => {
     try {
-        // const port = PORT.process.env || 3000;
-        const port = 3000;
+        const port = process.env.PORT || 3000;
 
+        await syncAndSeed()
         await app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
     }
     catch (err) {
