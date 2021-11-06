2608146afddcb1cbfc9140109900a295ad548a2c
jmartin-code <jmmartinez331@gmail.com>
2021-11-06 15:48:47 -0400

    modified package json

--- server/cron/index.js
+++ server/cron/index.js
@@ -4,7 +4,7 @@ const { models: { Item } } = require('../db');
 const scrapper = require('../scraper');
 
 const runCron = () => {
-    cron.schedule('* * * * *', async () => {
+    cron.schedule('* */3 * * *', async () => {
         try {
             console.log('cron running')
             const items = await Item.findAll() || [];
