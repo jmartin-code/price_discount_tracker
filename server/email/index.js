5338162ac3dd4072922334b5bcc54b870bae591a
jmartin-code <jmmartinez331@gmail.com>
2021-11-07 23:12:00 -0500

    added a limit of 30 characters to for the email subject

--- server/email/index.js
+++ server/email/index.js
@@ -15,7 +15,7 @@ const sendEmail = async (item, priceInfo, status) => {
         await transporter.sendMail({
             from: 'AmazonPriceMonitor@developer.com',
             to: `${item.email}`,
-            subject: `Amazon Price Monitor - ${item.name}`,
+            subject: `Amazon Price Monitor - ${item.name.substring(0, 30)}...`,
             text: `Amazon Price Monitor - ${item.name}`,
             html: `<body><h3>Amazon Price Monitor</h3> <p>${status} ${item.name}</p><h4>Target price: $${item.targetPrice}</h4><h4>Current Item price: $${item.price}</h4><h4>${priceInfo}</h4><p>Here is the link for the item: ${item.link}</p><p>*****Please do not reply to this email. This email is only used to provide automatic information.*****</p></body>`
         })
